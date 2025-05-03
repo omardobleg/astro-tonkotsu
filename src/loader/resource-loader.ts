6f95addef47f6821a846eb02c839f5a45f80cfd5
omargglez <omargglez@gmail.com>
2025-05-03 22:32:39 +0200

    feat: add backpu system por data

--- src/loader/resource-loader.ts
+++ src/loader/resource-loader.ts
@@ -1,5 +1,6 @@
 import type { TeableLoaderOptions } from "./teable-loader.types";
-
+import fs from "fs";
+const BACKUP_PATH = "./src/backup/table.json";
 export const tableLoader = (options: TeableLoaderOptions) => {
   const base = options.url;
   const table = options.table;
@@ -20,6 +21,15 @@ export const tableLoader = (options: TeableLoaderOptions) => {
     },
   })
     .then((response) => response.json())
+    .then((r) => {
+      fs.writeFileSync(BACKUP_PATH, JSON.stringify(r));
+      return r;
+    })
     .then((table) => table.records)
-    .catch((error) => console.error("Error:", error));
+    .catch((error) => {
+      console.error("Error:", error);
+      const resp = fs.readFileSync(BACKUP_PATH, "utf-8");
+      const object = JSON.parse(resp);
+      return object.records;
+    });
 };
